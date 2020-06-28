1006b2bc56661795f216706458fbed5127a6bf51
Ruth Mayodi <ruthmayodi@gmail.com>
2020-06-28 16:03:40 -0400

    completed part a

--- iss.py
+++ iss.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'ruthmayodi with the help of Howard Post'
+
+import requests
+import time
+import turtle
+import json
+
+
+def astronaut_list():
+    res = requests.get('http://api.open-notify.org/astros.json')
+    response_obj = json.loads(res.text)
+    print('There are currently {0} astronauts \
+in space. They are:'.format(response_obj['number']))
+    for person in response_obj['people']:
+        print('\t{0} who is on spacecraft: {1}'
+        .format(person['name'], person['craft']))
+
+
+
+def iss_location():
+    
+
+
+    
+
+
+
 
 
 def main():
-    pass
+    astronaut_list()
 
 
 if __name__ == '__main__':
